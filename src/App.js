b63e4809d519a698d3fdb52b314dda2640996f49
aereeeee <dofl5576@gmail.com>
2019-10-20 01:36:21 +0900

    [FEATURE] Ranking 컴포넌트 구현
    stargazers count로 sorting하여 리스트를 렌더한다

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const App = () => {
     repo: 0,
     star: 0
   });
+  const [ranking, setRanking] = useState([]);
 
   const inputHandler = e => {
     setUserName(e.target.value);
@@ -19,22 +20,24 @@ const App = () => {
 
   const clickHandler = async () => {
     const repoApi = `https://api.github.com/users/${userName}/repos`;
-    const repos = await axios.get(repoApi);
+    const { data } = await axios.get(repoApi);
 
     setState({
       name: userName,
-      repo: repos.length,
-      star: repos.reduce(
+      repo: data.length,
+      star: data.reduce(
         (acc, cur) => acc.stargazers_count + cur.stargazers_count
       )
     });
+
+    setRanking(data);
   };
 
   return (
     <div className="App">
       <Header inputHandler={inputHandler} clickHandler={clickHandler} />
       <UserInfo {...userState} />
-      <Ranking />
+      <Ranking ranking={ranking} />
     </div>
   );
 };
--- src/components/Ranking/Ranking.js
+++ src/components/Ranking/Ranking.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
-const Ranking = () => <div />;
+const Ranking = (props) => {
+  const { ranking } = props;
+  ranking.sort((a, b) => b.stargazers_count - a.stargazers_count);
 
+  return (
+    <>
+      <div />
+      {ranking.map((repo) => (
+        <div>
+          <span>{repo.name}</span>
+          <span>{repo.stargazers_count}</span>
+        </div>
+      ))}
+      <hr />
+    </>
+  );
+};
 export default Ranking;
